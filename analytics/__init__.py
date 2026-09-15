6084111f182f807a5978281a6ea41c2a0d635035
PikriNtr <fikrihaikal2101@gmail.com>
2026-09-15 12:47:59 +0800

    chore(analytics): export profile engine + profile chart

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,7 +1,8 @@
 from .calculator import calculate_astrite_cost
-from .charts import generate_pity_chart
+from .charts import generate_pity_chart, generate_profile_chart
 from .deep_stats import EARLY_PITY_THRESHOLD, calculate_deep_statistics
 from .pity import BANNER_NAMES, DEFAULT_GAME, GAME_BANNER_CONFIGS, calculate_pity_summary, get_game_banner_names
+from .profile import GAME_DISPLAY_NAMES, GAME_ORDER, unified_profile
 from .simulation import generate_simulation_chart, run_monte_carlo_simulation
 
 __all__ = [
@@ -12,6 +13,10 @@ __all__ = [
     "get_game_banner_names",
     "calculate_deep_statistics",
     "EARLY_PITY_THRESHOLD",
+    "unified_profile",
+    "GAME_ORDER",
+    "GAME_DISPLAY_NAMES",
+    "generate_profile_chart",
     "calculate_astrite_cost",
     "generate_pity_chart",
     "run_monte_carlo_simulation",
