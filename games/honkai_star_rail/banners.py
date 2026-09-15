3edda4d7ec6b3bbff0960d72f93c057f7131fe22
PikriNtr <fikrihaikal2101@gmail.com>
2026-09-15 10:39:34 +0800

    style(lint): sort imports and remove unused imports

--- games/honkai_star_rail/banners.py
+++ games/honkai_star_rail/banners.py
@@ -1,6 +1,5 @@
 from core.models import Banner
 
-
 HSR_BANNERS = {
     "1": Banner(id="1", game_id="honkai_star_rail", name="Character Event Warp", type="character", pity_cap=90, has_5050=True),
     "2": Banner(id="2", game_id="honkai_star_rail", name="Light Cone Event Warp", type="weapon", pity_cap=80, has_5050=True),
