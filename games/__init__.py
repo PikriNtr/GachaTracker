b00d22f39a3f24bc4f27f90401e63cc88140f119
PikriNtr <fikrihaikal2101@gmail.com>
2026-09-15 09:39:12 +0800

    feat(games): register_all_plugins registers all three games

--- games/__init__.py
+++ games/__init__.py
@@ -1,1 +1,13 @@
 # Games package initialization
+
+from games.wuthering_waves import WutheringWavesPlugin
+from games.genshin_impact import GenshinImpactPlugin
+from games.honkai_star_rail import HonkaiStarRailPlugin
+
+
+def register_all_plugins(registry) -> None:
+    """Registers every built-in game plugin into the given PluginRegistry."""
+    registry.register(WutheringWavesPlugin())
+    registry.register(GenshinImpactPlugin())
+    registry.register(HonkaiStarRailPlugin())
+
