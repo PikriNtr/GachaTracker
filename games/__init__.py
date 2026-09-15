54eafb3ab1586faaa80f4f93ef387067b13e35a8
PikriNtr <fikrihaikal2101@gmail.com>
2026-09-15 10:39:33 +0800

    style(lint): sort imports and remove unused imports

--- games/__init__.py
+++ games/__init__.py
@@ -1,8 +1,8 @@
 # Games package initialization
 
-from games.wuthering_waves import WutheringWavesPlugin
 from games.genshin_impact import GenshinImpactPlugin
 from games.honkai_star_rail import HonkaiStarRailPlugin
+from games.wuthering_waves import WutheringWavesPlugin
 
 
 def register_all_plugins(registry) -> None:
